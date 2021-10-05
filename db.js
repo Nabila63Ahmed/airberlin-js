c5f9ecdeb1dade28750e75feb62b258e2100691e
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2021-10-05 15:56:29 +0200

    Models creation

--- db.js
+++ db.js
@@ -1,24 +1,30 @@
 const { Timestamp } = require('bson')
-
-mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/airberlin')
 
-aircraftSchema = new mongoose.Schema({
+const aircraftSchema = new mongoose.Schema({
     tailNumber: String,
     model: String,
     seating: Array
 })
 
-airportSchema = new mongoose.Schema({
+const Aircraft =  mongoose.model('Aircraft', aircraftSchema)
+
+const airportSchema = new mongoose.Schema({
     iata: String,
     name: String
 })
 
-flightSchema = new mongoose.Schema({
+const Airport =  mongoose.model('Airport', airportSchema)
+
+
+const flightSchema = new mongoose.Schema({
     _id: String,
     aircraftTailNumber: String,
     departureAirportIata: String,
     arrivalAirportIata: String,
     departsAt: Timestamp
 })
+
+const Flight =  mongoose.model('Flight', flightSchema)
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-express = require ('express')
-server = express()
+const express = require ('express')
+
+const server = express()
 
 server.get('/ping', function(req, res) {
     res.send('pong')
 })
 
 server.listen(3000, function () {
+    console.log("Server is up and running on port 3000..")
 });
